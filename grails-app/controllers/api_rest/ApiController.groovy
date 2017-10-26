719fa793b8136a7cd02badc6cc5afd2c57cd7403
boucara <traorebelco@gmail.com>
2017-10-26 16:38:15 +0200

    le reste des services

--- grails-app/controllers/api_rest/ApiController.groovy
+++ grails-app/controllers/api_rest/ApiController.groovy
@@ -2,6 +2,7 @@ package api_rest
 
 import grails.converters.JSON
 import grails.converters.XML
+import grails.web.servlet.mvc.GrailsParameterMap
 
 class ApiController {
 
@@ -54,85 +55,39 @@ class ApiController {
                 if( livreInstance!=null) {
                     if(params.nom!=null && params.dateApparution==null && params.isbn==null && params.auteur==null  ) {
                         livreInstance.nom = params.nom
-                        if (livreInstance.save(flush:true) ){
-                            response.status = 200
-                            switch (request.getHeader('Accept')) {
-                                case 'JSON':
-                                    render livreInstance as JSON
-                                    break;
-                                case 'XML':
-                                    render livreInstance as XML
-                                    break;
-                            }
-                        } else
-                            response.status = 400
+
                     }
                      if(params.nom==null && params.dateApparution!=null && params.isbn==null && params.auteur==null  ) {
                         livreInstance.dateApparution = params.dateApparution
-                        if (livreInstance.save(flush:true) ){
-                            switch (request.getHeader('Accept')) {
-                                case 'JSON':
-                                    render livreInstance as JSON
-                                    break;
-                                case 'XML':
-                                    render livreInstance as XML
-                                    break;
-                                    response.status = 200
-                            }
-                        } else
-                            response.status = 400
+
                     }
                     if(params.nom==null && params.dateApparution==null && params.isbn!=null && params.auteur==null  ) {
                         livreInstance.isbn = params.isbn
-                        if (livreInstance.save(flush:true) ){
-                            switch (request.getHeader('Accept')) {
-                                case 'JSON':
-                                    render livreInstance as JSON
-                                    break;
-                                case 'XML':
-                                    render livreInstance as XML
-                                    break;
-                                    response.status = 200
-                            }
-                            response.status = 200
-                        } else
-                            response.status = 400
+
                     }
                      if(params.nom==null && params.dateApparution==null && params.isbn==null && params.auteur!=null  ) {
                         livreInstance.auteur = params.auteur
-                        if (livreInstance.save(flush: true)) {
-                            response.status = 200
-                            switch (request.getHeader('Accept')) {
-                                case 'JSON':
-                                    render livreInstance as JSON
-                                    break;
-                                case 'XML':
-                                    render livreInstance as XML
-                                    break;
-                                    response.status = 200
-                            }
-                        } else
-                            response.status = 400
+
                     }
                     if(params.nom!=null && params.dateApparution!=null && params.isbn!=null && params.auteur!=null  ) {
                         livreInstance.auteur = params.auteur
                         livreInstance.dateApparution= params.dateApparution
                         livreInstance.isbn=params.isbn
                         livreInstance.auteur=auteur
-                        if (livreInstance.save(flush: true)) {
-                            switch (request.getHeader('Accept')) {
-                                case 'JSON':
-                                    render livreInstance as JSON
-                                    break;
-                                case 'XML':
-                                    render livreInstance as XML
-                                    break;
-                                    response.status = 200
-                            }
-                            response.status = 200
-                        } else
-                            response.status = 400
+
                     }
+                    if (livreInstance.save(flush:true) ){
+                        response.status = 200
+                        switch (request.getHeader('Accept')) {
+                            case 'JSON':
+                                render livreInstance as JSON
+                                break;
+                            case 'XML':
+                                render livreInstance as XML
+                                break;
+                        }
+                    } else
+                        response.status = 400
                 }
                 break ;
                 case "DELETE":
@@ -156,6 +111,31 @@ class ApiController {
 
             default:
                 response.status = 405
+                break;
+        }
+
+    }
+    def livres(){
+        switch(request.getMethod()){
+            case "GET":
+                def livreInstance=Livre.getAll()
+                if(livreInstance!=null){
+                    switch (request.getHeader('Accept')) {
+                        case 'JSON':
+                            render livreInstance as JSON
+                            break;
+                        case 'XML':
+                            render livreInstance as XML
+                            break;
+
+
+                            response.status = 200
+                    }
+                }
+                else{
+                    response.status=400
+                    render " aucun livre"
+                }
                 break;
         }
 
@@ -204,52 +184,28 @@ class ApiController {
                 def bibliothequeInstance = Bibliotheque.get(params.bu.id);
                 if(params.nom!=null && params.adresse == null && params.anneConstructor == null){
                     bibliothequeInstance.nom=params.nom
-                    if(bibliothequeInstance.save(flush:true)){
-                        response.status=200
-                        switch (request.getHeader('Accept')) {
-                            case 'JSON':
-                                render  bibliothequeInstance as JSON
-                                break;
-                            case 'XML':
-                                render  bibliothequeInstance as XML
-                                break;
-                                response.status = 200
-                        }
-                    }
-                    else  response.status = 400
+
                 }
                 if(params.nom==null && params.adresse!= null && params.anneConstructor == null){
                     bibliothequeInstance.adresse=params.adresse
-                    if(bibliothequeInstance.save(flush:true)){
-                        response.status=200
-                        switch (request.getHeader('Accept')) {
-                            case 'JSON':
-                                render  bibliothequeInstance as JSON
-                                break;
-                            case 'XML':
-                                render  bibliothequeInstance as XML
-                                break;
-                                response.status = 200
-                        }
-                    }
-                    else  response.status = 400
+
                 }
                 if(params.nom==null && params.adresse == null && params.anneConstructor != null){
                     bibliothequeInstance.anneConstructor= Integer.parseInt(params.anneConstructor)
-                    if(bibliothequeInstance.save(flush:true)){
-                        response.status=200
-                        switch (request.getHeader('Accept')) {
-                            case 'JSON':
-                                render  bibliothequeInstance as JSON
-                                break;
-                            case 'XML':
-                                render  bibliothequeInstance as XML
-                                break;
-                                response.status = 200
-                        }
-                    }
-                    else  response.status = 400
                 }
+                if(bibliothequeInstance.save(flush:true)){
+                    response.status=200
+                    switch (request.getHeader('Accept')) {
+                        case 'JSON':
+                            render  bibliothequeInstance as JSON
+                            break;
+                        case 'XML':
+                            render  bibliothequeInstance as XML
+                            break;
+                            response.status = 200
+                    }
+                }
+                else  response.status = 400
 
                 break;
             case"DELETE":
@@ -268,28 +224,185 @@ class ApiController {
                 break;
         }
     }
-    def ressourcesLies(){
-        switch(request.getMethod()){
+    def librairies(){
+        switch (request.getMethod()){
             case"GET":
-                if(!Bibliotheque.get(params.bu.id)){
-                    render (status: 400, text:"cannot attach a librairie to a non existant bu(${params.bibliotheque.id})")
-                    return
-                }
-                def bibliothequeInstance = Bibliotheque.get(params.bu.id)
-                def livreInstance =bibliothequeInstance.getLivres()
-                    if(livreInstance!=null){
+                def bibliothequeInstance = Bibliotheque.getAll()
+                if(bibliothequeInstance!=null){
                     switch (request.getHeader('Accept')) {
                         case 'JSON':
-                            render  livreInstance as JSON
+                            render bibliothequeInstance as JSON
                             break;
                         case 'XML':
-                            render  livreInstance as XML
+                            render bibliothequeInstance as XML
+                            break;
+
+
+                            response.status = 200
+                    }
+                }
+                else {
+                    response.status=400
+                    render"aucune BU"
+                }
+                break;
+
+
+        }
+    }
+    def ressourcesLies(Bibliotheque  bibliothequeInstance){
+        switch(request.getMethod()) {
+            case "GET":
+                if (bibliothequeInstance == null) {
+                    render(status: 400, text: "cannot attach a librairie to a non existant")
+                    return
+                }
+
+                def livreInstance = bibliothequeInstance.getLivres()
+                if (livreInstance != null) {
+                    switch (request.getHeader('Accept')) {
+                        case 'JSON':
+                            render livreInstance as JSON
+                            break;
+                        case 'XML':
+                            render livreInstance as XML
                             break;
 
                     }
 
+
+                }
+            case "PUT":
+                if (bibliothequeInstance == null) {
+                    render(status: 400, text: "cannot attach a librairie to a non existant")
+                    return
                 }
 
+                def livreInstance= Livre.get(params.livre.id)
+                if (bibliothequeInstance.getLivres().contains(livreInstance)) {
+                    if (params.nom != null && params.dateApparution == null && params.isbn == null && params.auteur == null)
+                        livreInstance.nom = params.nom
+
+                    if (params.nom == null && params.dateApparution != null && params.isbn == null && params.auteur == null)
+                        livreInstance.dateApparution = params.dateApparution
+
+                    if (params.nom == null && params.dateApparution == null && params.isbn != null && params.auteur == null)
+                        livreInstance.isbn = params.isbn
+
+                    if (params.nom == null && params.dateApparution == null && params.isbn == null && params.auteur != null)
+                        livreInstance.auteur = params.auteur
+
+                    if (params.nom != null && params.dateApparution != null && params.isbn != null && params.auteur != null) {
+                        livreInstance.auteur = params.auteur
+                        livreInstance.dateApparution = params.dateApparution
+                        livreInstance.isbn = params.isbn
+                        livreInstance.auteur = auteur
+
+                    }
+
+                    if (livreInstance.save(flush: true)) {
+                        response.status = 200
+                        switch (request.getHeader('Accept')) {
+                            case 'JSON':
+                                render livreInstance as JSON
+                                break;
+                            case 'XML':
+                                render livreInstance as XML
+                                break;
+                        }
+                    } else
+                        response.status = 400
+
+
+                } else
+                    render "ce livre ne se trouve pas dans la bu identifiée"
+                break;
+
+            case"DELETE":
+
+                if(bibliothequeInstance==null){
+                    render(status: 400, text:"cannot attach a librairie to a non existant bu")
+                    return
+                }
+                def livreInstance = Livre.get(params.livre.id)
+                if(bibliothequeInstance.getLivres().contains(livreInstance)){
+                    bibliothequeInstance.getLivres().remove(livreInstance)
+                    livreInstance.delete(flush: true)
+                    response.status=200
+                    render"le livre a été bien supprimer dans cette BU"
+                }
+                else{
+                    reponse.status = 404
+                    render "la ressource à supprimer n'existe pas"
+
+                }
+                break;
+            case"POST":
+                if(bibliothequeInstance==null){
+                    render(status: 400, text:"cannot attach a librairie to a non existant bu")
+                    return
+                }
+                def livreInstance = new Livre(params)
+                if(bibliothequeInstance.addToLivres(livreInstance)){
+                    if(bibliothequeInstance.save(flush:true)){
+
+                        response.status=200
+                        switch (request.getHeader('Accept')) {
+                            case 'JSON':
+                                render livreInstance as JSON
+                                break;
+                            case 'XML':
+                                render livreInstance as XML
+                                break;
+                        }
+                    } else
+                        response.status=400
+                }
+                else render("erreur syntaxe")
+                break;
+
+                }
+
+
+
+
+
+
         }
+    def lectureLivreBu(Bibliotheque bibliothequeInstance){
+        switch(request.getMethod()) {
+            case"GET":
+                if(bibliothequeInstance==null){
+                    render(status: 400, text:"cannot attach a librairie to a non existant bu")
+                    return
+                }
+                def livreInstance =bibliothequeInstance.livres.find {it.id==Integer.parseInt(params.livre.id)}
+
+                    if(livreInstance!=null){
+                        response.status=200
+                        switch (request.getHeader('Accept')) {
+                            case 'JSON':
+                                render livreInstance as JSON
+                                break;
+                            case 'XML':
+                                render livreInstance as XML
+                                break;
+                        }
+                    } else
+                        response.status=400
+
+                break;
+
+        }
+
+
     }
-}
+
+
+    }
+
+
+
+
+
+
--- grails-app/controllers/api_rest/UrlMappings.groovy
+++ grails-app/controllers/api_rest/UrlMappings.groovy
@@ -8,6 +8,11 @@ class UrlMappings {
                 // apply constraints here
             }
         }
+        "/biblio/${id}/livres" (controller:"api",action:"ressourcesLies")
+        "/biblio/${id}/livre/" (controller:"api",action:"ressourcesLies")
+        "/biblio/${id}/livres/livre/" (controller:"api",action:"lectureLivreBu")
+        "/livre"(controller:"api",action:"livre")
+        "/bibliotheque"(controller:"api",action:"bibliotheque")
 
         "/"(view:"/index")
         "500"(view:'/error')
